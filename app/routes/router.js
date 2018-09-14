156f33ac4d25c336e29db5f009d94c2c36cec565
stephenwike <stephen.c.wise@gmail.com>
2018-09-13 23:54:54 -0600

    First Commit

---
+++ app/routes/router.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+router.use(function(req, res, next) {
+  console.log('Something is happening.');
+  next();
+});
+
+router.get('/', function(req, res) {
+	res.sendFile('views/home.html', { root : __dirname });
+});
+
+router.get('/config', function(req, res) {
+	res.sendFile('views/config.html', { root : __dirname });
+});
+
+router.get('/catan', function(req, res) {
+	res.sendFile('views/catan.html', { root : __dirname });
+});
+
+module.exports.router = router;
---
+++ server.js
@@ -0,0 +1,97 @@
+// set up ======================================================================
+var express = require('express');
+var tv_app = express(); // GAME CENTRAL KIOSK
+var an_app = express(); // ANDROID APP
+var tv_server = require('http').Server(tv_app);
+var an_server = require('http').Server(an_app);
+var tv_io = require('socket.io')(tv_server);
+var an_io = require('socket.io')(an_server);
+
+// configuration ===============================================================
+var tv_app_port = 3000;
+var an_app_port = 4000;
+
+// socket io +===================================================================
+tv_io.on('connection', (socket) => 
+{	
+	console.log("TV connected, id: " + socket.id);
+	
+	// Start listening for android apps
+	an_server.listen(an_app_port, () => {
+		console.log("Listening on port " + an_app_port);
+	});
+	
+	socket.on('disconnect', (args) =>
+	{
+		// Close server for android app until tv reconnects
+		console.log("TV disconnected, id: " + socket.id);
+		//an_server.close();
+		//console.log("Closing app server");
+	});
+	
+	// Forward all events to android(s)
+	var onevent = socket.onevent;
+	socket.onevent = function (packet) 
+	{
+		var args = packet.data || [];
+		onevent.call (this, packet);    // original call
+		packet.data = ["*"].concat(args);
+		onevent.call(this, packet);      // additional call to catch-all
+	};
+	
+	//socket.on('*', (evt, data) =>
+	//{
+	//	console.log("EVENT " + evt + ", DATA: "); 
+	//	console.log(data);
+	//	if (data["id"] == "*")
+	//	{
+	//		an_io.emit(evt, data);
+	//	}
+	//	else
+	//	{
+	//		an_io.to(data["id"]).emit(evt, data);
+	//	}
+	//});
+});
+
+an_io.on('connection', (socket) => 
+{
+	console.log("App connected, id: " + socket.id);
+	//loginCount++;
+	var args = { "id": socket.id };
+	tv_io.emit('newconnect', args);
+	
+	socket.on('disconnect', (args) =>
+	{
+		loginCount--;
+		args = { "count": loginCount };
+		console.log("App disconnected, id: " + socket.id);
+		tv_io.emit('disconnect', args);
+	});
+	
+	// Forward all events to tv
+	var onevent = socket.onevent;
+	socket.onevent = function (packet) 
+	{
+		var args = packet.data || [];
+		onevent.call (this, packet);    // original call
+		packet.data = ["*"].concat(args);
+		onevent.call(this, packet);      // additional call to catch-all
+	};
+	
+	socket.on('*', (evt, data) =>
+	{
+		console.log("EVENT " + evt + ", DATA: "); 
+		console.log(data);
+		tv_io.emit(evt, data);
+	});
+});
+
+// routes =======================================================================
+tv_app.use(express.static('app'));
+tv_app.use('/', require('./app/routes/router').router);
+
+// listen (start app with node server.js) =======================================
+tv_server.listen(tv_app_port, () => {
+  console.log("Listening on port " + tv_app_port);
+});
